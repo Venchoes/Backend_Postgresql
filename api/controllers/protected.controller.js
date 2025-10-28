5bad7434a7424e6431f26f7b8b113d1f52f21470
Felipe Almeida <165962859+Venchoes@users.noreply.github.com>
2025-10-28 22:57:34 +0000

    fix

--- api/controllers/protected.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.protectedRouteHandler = void 0;
-const protectedRouteHandler = (req, res) => {
-    const user = req.user;
-    console.log('[PROTECTED] Acesso autorizado para usuário:', user?.email || user?.id);
-    res.status(200).json({ message: 'Acesso autorizado' });
-};
-exports.protectedRouteHandler = protectedRouteHandler;
